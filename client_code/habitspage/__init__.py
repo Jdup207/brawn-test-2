305abc15fcbeeadf045977ccd0cc0e4b03b6668b
james.dupreez@kingsway.school.nz <james.dupreez@kingsway.school.nz>
2024-08-15 23:23:09 +0000

    Edited ServerModule1, habitspage, index, logactivitesprompt and 4 more

--- client_code/habitspage/__init__.py
+++ client_code/habitspage/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import habitspageTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 # Any code you write here will run before the form opens.
