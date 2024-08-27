142b6a25c226c8a02ea82874ea8afd879798ffdb
james.dupreez@kingsway.school.nz <james.dupreez@kingsway.school.nz>
2024-08-27 02:59:27 +0000

    Edited sportpage

--- client_code/sportpage/__init__.py
+++ client_code/sportpage/__init__.py
@@ -11,7 +11,7 @@ class sportpage(sportpageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.repeating_panel_1.items = app_tables.activites.search()
+    # self.repeating_panel_1.items = app_tables.activites.search()
   
   # How to get back to Home page
   def home_button_click(self, **event_args):
