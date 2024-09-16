a8b01da93696c68be0611bb920d5bd646c8d5e80
james.dupreez@kingsway.school.nz <james.dupreez@kingsway.school.nz>
2024-09-16 08:51:22 +0000

    Edited managementpage

--- client_code/managementpage/__init__.py
+++ client_code/managementpage/__init__.py
@@ -1,4 +1,4 @@
-  from ._anvil_designer import managementpageTemplate
+from ._anvil_designer import managementpageTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
